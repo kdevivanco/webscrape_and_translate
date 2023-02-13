638411e709c40a3439fae798d7429df836016e1a
kdevivanco <kdevivanco@gmail.com>
2023-02-13 17:32:36 -0500

    translates correctly

--- website/server.py
+++ website/server.py
@@ -1,7 +1,6 @@
 import random
 import json
 from flask import Flask, render_template, request, redirect, session 
-from scripts import scrapeimg
 
 app = Flask(__name__)
 
@@ -9,7 +8,17 @@ app = Flask(__name__)
 def users_page():
     return render_template('index.html')
 
+@app.route('/uni')
+def show_best_unis():
+    return render_template('universities.html')
 
+@app.route('/subjects')
+def show_subjects():
+    return render_template('subjects.html')
+
+@app.route('/pc_free_certificates')
+def pc_free_certificates():
+    return render_template('pc_free_certificates.html')
 
 
 if __name__ == "__main__":
